09b69625b361cea55e5d8f8a0617beb0e2f8c915
Maxi Stauss <Maxi Stauss>
2016-04-29 20:23:19 +0200

    continued install script

--- tools/scripts/bcsinstall.sh
+++ tools/scripts/bcsinstall.sh
@@ -9,7 +9,7 @@ echo "variables are:"
 echo "BCS=$BCS"
 echo "JAVA=$JAVA_HOME"
 echo "TOMCAT=$TOMCAT_HOME"
-echo "BCS_VERSION=$BCS_VERSION"
+echo "BCS_VERSION=$PROJEKTRON_VERSION"
 
 projektron_minor=$PROJEKTRON_VERSION
 projektron_major=${projektron_minor:0:4}
@@ -20,3 +20,9 @@ echo "Major Projektron Version=$projektron_major"
 # Download url is this
 URL=https://support.projektron.de/webdav/BCS/Projekte/Downloads/Projektron_BCS_$projektron_major/$projektron_minor/.Dateien/projektron-bcs-$projektron_minor.zip
 echo "Download URL=$URL"
+# Credentials are stored in /scripts/credentials
+credfile=/scripts/credentials
+USERNAME=$(sed '1q;d' credfile)
+PASSWORD=$(sed '2q;d' credfile)
+# Download specified Projektron Version to BCS directory
+wget --user=$USERNAME --password=$PASSWORD  $URL $BCS
