ff096700d1dc539ff1872be4b90d40e47ff88528
Maxi Stauss <Maxi Stauss>
2016-04-30 21:25:24 +0200

    Updated script, should now run infinitely

--- tools/scripts/bcsinstall.sh
+++ tools/scripts/bcsinstall.sh
@@ -20,9 +20,16 @@ echo "Major Projektron Version=$projektron_major"
 # Download url is this
 URL=https://support.projektron.de/webdav/BCS/Projekte/Downloads/Projektron_BCS_$projektron_major/$projektron_minor/.Dateien/projektron-bcs-$projektron_minor.zip
 echo "Download URL=$URL"
-# Credentials are stored in /scripts/credentials
+# Credentials are stored in /scripts/credentials, first line is username, second line is password
 credfile=/scripts/credentials
 USERNAME=$(sed '1q;d' credfile)
 PASSWORD=$(sed '2q;d' credfile)
 # Download specified Projektron Version to BCS directory
 wget --user=$USERNAME --password=$PASSWORD  $URL $BCS
+# Unzip downloaded file and remove zip file
+unzip projektron-bcs-$projektron_minor.zip $BCS
+rm projektron-bcs-$projektron_minor.zip
+
+# Last step start tomcat server
+cd $TOMCAT_HOME/bin
+sh startup.sh
